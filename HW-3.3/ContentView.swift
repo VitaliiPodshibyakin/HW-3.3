7924c69cf8ac533117d58bcba1d325665b96d140
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-05-15 20:30:46 +0300

    Initial Commit

---
+++ HW-3.3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW-3.3
+//
+//  Created by Виталий Подшибякин on 15.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW-3.3/HW_3_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_3App.swift
+//  HW-3.3
+//
+//  Created by Виталий Подшибякин on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
